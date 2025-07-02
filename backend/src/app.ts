223d17e2df8a4f57391485b31091faaee32ed571
Krishna8665 <khatrikrissna11@gmail.com>
2025-07-02 23:51:33 +0545

    models

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import globalErrorHandler from './middlewares/globalErrorHandler'
 const app = express()
 
 
 
-
+app.use(globalErrorHandler)// app.ts ma reagister garne, error handling function chai every time call vairako huncha hai,katai pati kasai le req garyo req garda error aayo vane yo call huncha yesle everytime project watch gari rako huncna
 
 
 
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,7 +3,11 @@ config()
 
 const envConfig = {
     port: process.env.PORT,
-    mongodbString: process.env.MONGODB_URI
+    mongodbString: process.env.MONGODB_URI,
+    backendUrl : process.env.BACKEND_URL,
+    environment : process.env.NODE_ENV
+
+
 
 }
 
